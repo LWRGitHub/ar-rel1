3f5a8df8d53df3adc72abd2158ea3f131069c66a
Ubuntu <ubuntu@ip-172-31-95-21.ec2.internal>
2020-03-05 01:24:33 +0000

    ar-rel1 app rb update get post

--- app.rb
+++ app.rb
@@ -3,3 +3,11 @@ require "sinatra/reloader"
 require "sinatra/activerecord"
 set :database, { adapter: "sqlite3", database: "mydb.db" }
 require './models/user'
+
+get '/' do
+    @users = User.all
+    rb :seeds
+end
+
+post '/' do
+end
