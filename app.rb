2cee7fde0dd4f2dd2a02079d37dddaed7f27a84d
Ubuntu <ubuntu@ip-172-31-95-21.ec2.internal>
2020-03-14 23:07:31 +0000

    User & 1 & 2 & app update

--- app.rb
+++ app.rb
@@ -3,11 +3,34 @@ require "sinatra/reloader"
 require "sinatra/activerecord"
 set :database, { adapter: "sqlite3", database: "mydb.db" }
 require './models/user'
+require './models/team'
 
 get '/' do
-    @users = User.all
-    rb :seeds
+    User.all.to_yam1 + Team.all.to_yam1
 end
 
-post '/' do
+get '/users1' do
+   @users = User.all
+   erb :users1
+end
+
+get '/users2' do
+   @users = User.all
+   erb :users2
+end
+
+post '/show_user' do
+    @user = User.find_by(name: params[:name])
+    if @user.nil?
+        return "User not found"
+    end
+    erb :user
+end
+
+get '/user/:id' do
+   @user = User.find(params[:id])
+   if @user.nil?
+       return "User not found"
+   end
+   erb :user
 end
